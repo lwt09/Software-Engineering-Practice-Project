d996ab5afd3a587fb9871128184a0feb2970f5e5
ysj <1584809881@qq.com>
2019-11-12 20:07:42 +0800

    springboot环境搭建，简单接口编写

---
+++ 后端/src/main/java/com/example/datajdbc/bean/User.java
@@ -0,0 +1,40 @@
+package com.example.datajdbc.bean;
+
+public class User {
+    private Integer userId;
+    private String userName;
+    private String userPsd;
+    private String userSex;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPsd() {
+        return userPsd;
+    }
+
+    public void setUserPsd(String userPsd) {
+        this.userPsd = userPsd;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+}
